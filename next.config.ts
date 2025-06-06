b92e7295e150994ecd86531bfc5cf83e722f112c
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2025-06-06 09:35:28 +0700

    chore: update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,21 @@ import type { NextConfig } from "next"
 
 import redirects from "@/lib/utils/redirects"
 
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
 const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+                const url = new URL(item)
+
+                return {
+                    hostname: url.hostname,
+                    protocol: url.protocol.replace(':', '') as 'http' | 'https',
+                }
+            }),
+        ],
+    },
     reactStrictMode: true,
     redirects,
 }
